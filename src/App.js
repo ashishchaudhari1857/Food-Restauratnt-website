c8168acc0ffd38b1ccc210e34f3cb1079d42d8b0
Ashish Chaudhari <ashishchaudharistudy1857@gmail.com>
2023-06-03 10:20:59 +0530

    Add Summary Block

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import react  from "react";
+import {Header}from "./Components/Layout/Header";
+import { MealSummary } from "./Components/Meal/MealSummary";
+const App=()=>{
+
+  
+  return(
+    <>
+    <Header></Header>
+<MealSummary></MealSummary>
+    </>
+  )
+}
+
+export default App;
