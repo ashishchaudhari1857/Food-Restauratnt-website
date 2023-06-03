827722ad229c3bf9e86032bcb39e543051b86a43
Ashish Chaudhari <ashishchaudharistudy1857@gmail.com>
2023-06-03 11:38:27 +0530

    CHECK

--- src/Components/Meal/MealSummary.js
+++ src/Components/Meal/MealSummary.js
@@ -4,6 +4,7 @@ const MealSummary =()=>{
     return (
         <>
         <div className={classes.summary}>
+            <h1>dabsdjkbajd</h1>
             <h2>Delicious Food ,Delivered To You</h2>
             <p>choose your favourtie food from our board of selection of avialable meal and enjoy a Delicious lunch of at home</p>
             <p>choose your favourtie food from our board of selection of avialable meal and enjoy a Delicious lunch of at home</p>
