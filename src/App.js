1ae9de17fd650c55528d9398f834ea0c92967320
Ashish Chaudhari <ashishchaudharistudy1857@gmail.com>
2023-06-04 22:02:04 +0530

    add cart with my approch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import react  from "react";
 import {Header}from "./Components/Layout/Header";
 import { Meal } from "./Components/Meal/Meal";
+import Cart from "./Components/Cart/Cart";
 const App=()=>{
 
   
   return(
     <>
     <Header></Header>
+    <Cart></Cart>
    <main>
-
     <Meal></Meal>
    </main>
     </>
