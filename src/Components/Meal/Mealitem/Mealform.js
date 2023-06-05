0685bb0a93419ee5dc0532e847039e0f1258c61c
Ashish Chaudhari <ashishchaudharistudy1857@gmail.com>
2023-06-05 22:58:48 +0530

    TRY TO ADD   THE  ITEMS IN CART

--- src/Components/Meal/Mealitem/Mealform.js
+++ src/Components/Meal/Mealitem/Mealform.js
@@ -1,9 +1,27 @@
 import classes from "./Mealform.module.css";
 import Input from "../../UI/Input";
+import { useContext } from "react";
+import { CartContext } from "../../../Store/CartContext";
 const MealForm = (props) => {
+  const ctx = useContext(CartContext);
+  const ADD_ITEM = (event) => {
+    event.preventDefault();
+    console.log(ctx);
+    const obj = {
+      quantity: event.target.quantity.value,
+    };
+    console.log(obj.quantity);
+    console.log(ctx.items.id)
+    if(ctx.items.id){
+      console.log("csnk")
+    }
+    else{
+         ctx.items.push({id:"c1"})
+    }
+  };
   return (
     <>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={ADD_ITEM}>
         <Input
           label={"quantity"}
           input={{
@@ -13,9 +31,10 @@ const MealForm = (props) => {
             max: "5",
             step: "1",
             defaultValue: "1",
+            name: "quantity",
           }}
         ></Input>
-        <button >Add item</button>
+        <button type="submit">Add item</button>
       </form>
     </>
   );
